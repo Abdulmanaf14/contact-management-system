e08af4a587a8aa857b9d0d32d516f007cd1244c9
abdul manaf <abdul.manaf@fedo.ai>
2024-05-29 16:47:29 +0530

    db update

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { UsersModule } from './components/users/users.module';
 import { ContactsModule } from './components/contacts/contacts.module';
 import { User } from './components/users/entities/user.entity';
 import { Contact } from './components/contacts/entities/contact.entity';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import {  SessionModule } from 'nestjs-session';
 import session from 'express-session';
 import { SECRET_KEY } from './constants';
@@ -16,15 +16,15 @@ import { SECRET_KEY } from './constants';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'nizamijju',
-      database: 'contacts_db',
-      entities: [User, Contact],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('postgres://default:6RiE9VxzpeNI@ep-ancient-bread-a4btpvf3.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require'),
+        entities: [User, Contact],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     UsersModule, ContactsModule, SessionModule.forRoot({
       session: { secret: 'contact use' },
